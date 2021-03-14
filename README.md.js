5477703ac3e74f6541bcda69f19390ef26082d32
RamiReddy Ganapa <80382647+Ramu4444@users.noreply.github.com>
2021-03-14 19:40:54 +0530

    Create README.md.js

---
+++ README.md.js
@@ -0,0 +1,7 @@
+# RamiReddy-Assignment.js
+// WThis Code is about how to extract values from the given website
+// We have extracted the PUBLICATION DATE
+// We have extracted the BIDDING DATE
+// We have extracted the OBJECTS
+// and we also extracted the LINKS using Selectors
+// We can run this code in CHROME CONSOLE or in an Javascript console by Webpage Loader
